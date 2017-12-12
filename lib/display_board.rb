4081894730f7213718ac4206b14eec066ee63820
Azumi Callanan <azucm@hotmail.com>
2017-12-12 00:49:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -16,11 +16,11 @@ board11 = [ "O" , "O" , "O" , "O" , "O" , "O" , "O" , "O" , "O" ]
 
 
 def display_board(board)
-  puts "#{board[0]}|#{board[1]}|#{board[2]}
+  puts " #{board[0]} | #{board[1]} | #{board[2]} 
   \n-----------\n
-  #{board[3]}|#{board[4]}|#{board[5]}
+   #{board[3]} | #{board[4]} | #{board[5]} 
   \n-----------\n
-  #{board[6]}|#{board[7]}|#{board[8]}
+   #{board[6]} | #{board[7]} | #{board[8]} 
   \n"
 end
 
